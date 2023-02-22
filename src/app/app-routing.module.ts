7a6da85f99435c0b928d87d94e4a1122f8a59e90
[oksana-furman] <[oksan9a.furman@gmail.com]>
2023-02-22 13:55:39 +0100

    add nav, footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsListComponent } from './products-list/products-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "", component: HomeComponent
+}, {
+  path: "products", component: ProductsListComponent
+}, {
+  path: "cart", component: CartComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
